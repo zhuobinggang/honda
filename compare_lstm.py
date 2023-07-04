b12ab0939b2425a28d0369056e464bde762563e4
zhuobinggang <zhuobinggang@gmail.com>
2023-07-04 14:54:31 +0900

    准备实验BILSTM+CRF

--- compare_lstm.py
+++ compare_lstm.py
@@ -23,14 +23,11 @@ class BILSTM(nn.Module):
         self.BCE = nn.BCELoss()
         self.cuda()
     def get_titles_from_input(self, item):
-        tokens, labels, titles, paras = item
-        return titles
+        return item[2]
     def get_labels_from_input(self, item):
-        tokens, labels, titles, paras = item
-        return labels
+        return item[1]
     def get_tokens_from_input(self, item):
-        tokens, labels, titles, paras = item
-        return tokens
+        return item[0]
     def forward(self, item):
         tokens = self.get_tokens_from_input(item)
         out_ft = torch.stack([torch.from_numpy(self.ft.get_word_vector(str(word))) for word in tokens]).cuda() # (seq_len, 300)
@@ -103,6 +100,10 @@ def run_batch(seed = 10, indexs = range(3), mtype = 0):
                 print('BILSTM_TITLE')
                 m = BILSTM_TITLE()
                 train_and_save_checkpoints(m, f'BILSTM_TITLE_RP{repeat}_DS{idx}', ds_train, ds_dev, ds_test, check_step = 300, total_step = 30000)
+            elif mtype == 2:
+                print('BILSTM_TITLE_CRF')
+                m = BILSTM_TITLE_CRF()
+                train_and_save_checkpoints(m, f'BILSTM_TITLE_CRF_RP{repeat}_DS{idx}', ds_train, ds_dev, ds_test, check_step = 300, total_step = 30000)
 
 def script():
     run_batch(mtype = 0) # BILSTM
@@ -133,5 +134,49 @@ class BILSTM_TITLE(BILSTM):
         out_mlp = out_mlp.view(-1)  # (seq_len)
         return out_mlp
 
+
+class BILSTM_TITLE_CRF(BILSTM_TITLE):
+    def init_all(self):
+        self.ft = fasttext.load_model('/home/taku/cc.ja.300.bin')
+        self.tagger = Tagger('-Owakati')
+        self.rnn = nn.LSTM(302, 302, 1, batch_first = True, bidirectional = True)
+        self.classifier = nn.Sequential(
+            nn.Linear(604, 100),
+            nn.LeakyReLU(0.1),
+            nn.Linear(100, 2),
+        )
+        self.crf = CRF(2, batch_first=True)
+        self.cuda()
+    def forward(self, item):
+        out_ft = self.transfer_words(item) # (seq_len, 302)
+        out_rnn, (_, _) = self.rnn(out_ft.cuda()) # (?, 604)
+        out_mlp = self.classifier(out_rnn)  # (seq_len, 2)
+        return out_mlp
+    def loss(self, item):
+        labels = self.get_labels_from_input(item)
+        out_mlp = self.forward(item)  # (seq_len, 2)
+        out_mlp = out_mlp.unsqueeze(0) # (1, seq_len, 2)
+        tags = t.LongTensor([self.get_labels_from_input(item)]).cuda()
+        loss = -self.crf(out_mlp, tags)
+        return loss
+    def emphasize(self, item):
+        out_mlp = self.forward(item)  # (seq_len, 2)
+        out_mlp = out_mlp.unsqueeze(0)
+        out_mlp = self.crf.decode(out_mlp)[0]
+        res = [True if res > 0.5 else False for res in out_mlp]
+        return res
+    def test(self, ds):
+        target_all = []
+        result_all = []
+        for item in ds:
+            out_mlp = self.forward(item)  # (seq_len, 2)
+            out_mlp = out_mlp.unsqueeze(0)
+            out_mlp = self.crf.decode(out_mlp)[0] # (seq_len, 0/1)
+            target_all.append(self.get_labels_from_input(item))
+            result_all.append(out_mlp)
+        # flatten & calculate
+        results = flatten(result_all)
+        targets = flatten(target_all)
+        return cal_prec_rec_f1_v2(results, targets)
 
 
