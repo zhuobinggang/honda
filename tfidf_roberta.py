8206afc640a1c265af9877c01ca0c26ccc93a1ea
zhuobinggang <zhuobinggang@gmail.com>
2024-04-26 15:46:11 +0900

    带上crf再跑看看，大概率没希望

--- tfidf_roberta.py
+++ tfidf_roberta.py
@@ -1,4 +1,4 @@
-from roberta import Sector_Roberta_Title_Append, encode_title_append, roberta_encode_token, encode_plus, Sector_Roberta
+from roberta import encode_title_append, roberta_encode_token, encode_plus, Sector_Roberta, Sector_Roberta_Title_Append_Crf
 import torch
 from common import flatten, train_and_save_checkpoints, cal_prec_rec_f1_v2, topk_tokens
 
@@ -12,6 +12,7 @@ def case_print(item):
     ids, heads = encode_title_append_topk_tokens_append(toker, tokens, title, top3tokens)
     return toker.decode(ids)
 
+# NOTE: most crucial function
 # printed: [CLS] まだまだ寒い日が続きますが、本格的な春は刻 一刻と近づいてきています。[SEP] 辛い症状に負けないための「花粉症・アレルギー対策」特集を行います![SEP] 刻,一刻,春[SEP]
 def encode_title_append_topk_tokens_append(toker, tokens, title, topk_tokens):
     # title encode
@@ -39,11 +40,24 @@ class Sector_Roberta_Title_Append_Tfidf_Append(Sector_Roberta):
         tokens = item[0]
         return topk_tokens(tokens, tfidf_scores, 3)
     def get_ids_and_heads(self, item):
-        ids, heads = encode_title_append_topk_tokens_append(self.toker, self.get_tokens(item), self.get_title(item), self.get_top3_tfidf_tokens(item)) # With title
+        ids, heads = encode_title_append_topk_tokens_append(self.toker, self.get_tokens(item), self.get_title(item), self.get_top3_tfidf_tokens(item)) # With title, top3 tokens
+        return ids, heads
+
+
+# With CRF
+class Sector_Roberta_Title_Append_Crf_Tfidf_Append(Sector_Roberta_Title_Append_Crf):
+    def get_title(self, item):
+        return item[4] # tokens, labels, is_title, para_first, title, tf_idf
+    def get_top3_tfidf_tokens(self, item):
+        tfidf_scores = item[5] # tokens, labels, is_title, para_first, title, tf_idf
+        tokens = item[0]
+        return topk_tokens(tokens, tfidf_scores, 3)
+    def get_ids_and_heads(self, item):
+        ids, heads = encode_title_append_topk_tokens_append(self.toker, self.get_tokens(item), self.get_title(item), self.get_top3_tfidf_tokens(item)) # With title, top3 tokens
         return ids, heads
 
 # 2024.4.24：跑一遍看看Sector_Roberta_Title_Append_Tfidf_Append的性能，应该是每个split三次就可以了吧
-def run():
+def run_without_crf():
     from tfidf import read_and_build_dataset
     for repeat in range(3):
         for split in range(5):
@@ -57,6 +71,19 @@ def run():
             m = Sector_Roberta_Title_Append_Tfidf_Append()
             train_and_save_checkpoints(m, f'ROBERTA_TITLE_APPEND_TFIDF_RP{repeat}_DS{split}', trainset, devset, testset, check_step = 300, total_step = 3000)
 
-
+# 2024.4.26
+def run_with_crf():
+    from tfidf import read_and_build_dataset
+    for repeat in range(3):
+        for split in range(5):
+            # Dataset prepare
+            train_dev_set, test_set = read_and_build_dataset(split)
+            train_dev_set = flatten(train_dev_set)
+            trainset = train_dev_set[:-500]
+            devset = train_dev_set[-500:]
+            testset = flatten(test_set)
+            # Train
+            m = Sector_Roberta_Title_Append_Crf_Tfidf_Append()
+            train_and_save_checkpoints(m, f'ROBERTA_TITLE_APPEND_CRF_TFIDF_RP{repeat}_DS{split}', trainset, devset, testset, check_step = 300, total_step = 3000)
 
 
